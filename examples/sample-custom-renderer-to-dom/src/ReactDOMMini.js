6f7a47eed1ab4e0699baeb3bfa2e70db7f723c6d
SyMind <dacongsama@live.com>
2021-06-19 15:39:19 +0800

    format code

--- examples/sample-custom-renderer-to-dom/src/ReactDOMMini.js
+++ examples/sample-custom-renderer-to-dom/src/ReactDOMMini.js
@@ -4,74 +4,74 @@ const rootHostContext = {};
 const childHostContext = {};
 
 const hostConfig = {
-    now: Date.now,
-    getRootHostContext: () => {
-        return rootHostContext;
-    },
-    prepareForCommit: () => {},
-    resetAfterCommit: () => {},
-    getChildHostContext: () => {
-        return childHostContext;
-    },
-    shouldSetTextContent: (type, props) => {
-        return typeof props.children === 'string' || typeof props.children === 'number';
-    },
-    createInstance: (type, newProps, rootContainerInstance, _currentHostContext, workInProgress) => {
-        const domElement = document.createElement(type);
-        Object.keys(newProps).forEach(propName => {
-        const propValue = newProps[propName];
-        if (propName === 'children') {
-            if (typeof propValue === 'string' || typeof propValue === 'number') {
-            domElement.textContent = propValue;
-            }
-        } else if (propName === 'onClick') {
-            domElement.addEventListener('click', propValue);
-        } else if (propName === 'className') {
-            domElement.setAttribute('class', propValue);
-        } else {
-            const propValue = newProps[propName];
-            domElement.setAttribute(propName, propValue);
+  now: Date.now,
+  getRootHostContext: () => {
+    return rootHostContext;
+  },
+  prepareForCommit: () => { },
+  resetAfterCommit: () => { },
+  getChildHostContext: () => {
+    return childHostContext;
+  },
+  shouldSetTextContent: (type, props) => {
+    return typeof props.children === 'string' || typeof props.children === 'number';
+  },
+  createInstance: (type, newProps, rootContainerInstance, _currentHostContext, workInProgress) => {
+    const domElement = document.createElement(type);
+    Object.keys(newProps).forEach(propName => {
+      const propValue = newProps[propName];
+      if (propName === 'children') {
+        if (typeof propValue === 'string' || typeof propValue === 'number') {
+          domElement.textContent = propValue;
         }
-        });
-        return domElement;
-    },
-    createTextInstance: text => {
-        return document.createTextNode(text);
-    },
-    appendInitialChild: (parent, child) => {
-        parent.appendChild(child);
-    },
-    appendChild(parent, child) {
-        parent.appendChild(child);
-    },
-    finalizeInitialChildren: () => {},
-    supportsMutation: true,
-    appendChildToContainer: (parent, child) => {
-        parent.appendChild(child);
-    },
-    prepareUpdate(domElement, oldProps, newProps) {
-        return true;
-    },
-    commitUpdate(domElement, updatePayload, type, oldProps, newProps) {
-        Object.keys(newProps).forEach(propName => {
-            const propValue = newProps[propName];
-            if (propName === 'children') {
-                if (typeof propValue === 'string' || typeof propValue === 'number') {
-                    domElement.textContent = propValue;
-                }
-            } else {
-                const propValue = newProps[propName];
-                domElement.setAttribute(propName, propValue);
-            }
-        });
-    },
-    commitTextUpdate(textInstance, oldText, newText) {
-        textInstance.text = newText;
-    },
-    removeChild(parentInstance, child) {
-        parentInstance.removeChild(child);
-    },
-    clearContainer: () => {}
+      } else if (propName === 'onClick') {
+        domElement.addEventListener('click', propValue);
+      } else if (propName === 'className') {
+        domElement.setAttribute('class', propValue);
+      } else {
+        const propValue = newProps[propName];
+        domElement.setAttribute(propName, propValue);
+      }
+    });
+    return domElement;
+  },
+  createTextInstance: text => {
+    return document.createTextNode(text);
+  },
+  appendInitialChild: (parent, child) => {
+    parent.appendChild(child);
+  },
+  appendChild(parent, child) {
+    parent.appendChild(child);
+  },
+  finalizeInitialChildren: () => { },
+  supportsMutation: true,
+  appendChildToContainer: (parent, child) => {
+    parent.appendChild(child);
+  },
+  prepareUpdate(domElement, oldProps, newProps) {
+    return true;
+  },
+  commitUpdate(domElement, updatePayload, type, oldProps, newProps) {
+    Object.keys(newProps).forEach(propName => {
+      const propValue = newProps[propName];
+      if (propName === 'children') {
+        if (typeof propValue === 'string' || typeof propValue === 'number') {
+          domElement.textContent = propValue;
+        }
+      } else {
+        const propValue = newProps[propName];
+        domElement.setAttribute(propName, propValue);
+      }
+    });
+  },
+  commitTextUpdate(textInstance, oldText, newText) {
+    textInstance.text = newText;
+  },
+  removeChild(parentInstance, child) {
+    parentInstance.removeChild(child);
+  },
+  clearContainer: () => { }
 };
 const ReactReconcilerInst = ReactReconciler(hostConfig);
 
