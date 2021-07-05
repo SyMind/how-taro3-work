907f419eaa23a577084f55ffbfbfb2957b2f32da
SyMind <dacongsama@live.com>
2021-07-05 09:12:03 +0800

    add event handle

--- examples/sample-custom-renderer-to-swan/pages/index/index.js
+++ examples/sample-custom-renderer-to-swan/pages/index/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { createSwanPage } from '../../../../lib/runtime';
 
 function SwanPage() {
-  return React.createElement('view', null, 'hello, world')
+  const [value, setValue] = useState('333')
+  console.log('value', value)
+
+  return React.createElement(
+    'view',
+    null,
+    React.createElement('input', {
+      onInput: event => setValue(event.detail.value)
+    }),
+    React.createElement('view', null, value)
+  )
 }
 
 Page(createSwanPage(SwanPage))
