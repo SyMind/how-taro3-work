c0460e490cf6d56820c74106b865b19cdf991ce4
Cong-Cong Pan <dacongsama@live.com>
2022-02-05 14:07:58 +0800

    Update ReactDOMMini.js

--- examples/sample-custom-renderer-to-dom/src/ReactDOMMini.js
+++ examples/sample-custom-renderer-to-dom/src/ReactDOMMini.js
@@ -4,19 +4,22 @@ const rootHostContext = {};
 const childHostContext = {};
 
 const hostConfig = {
-  now: Date.now,
-  getRootHostContext: () => {
+  getRootHostContext() {
     return rootHostContext;
   },
-  prepareForCommit: () => { },
-  resetAfterCommit: () => { },
-  getChildHostContext: () => {
+  prepareForCommit() {
+
+  },
+  resetAfterCommit() {
+
+  },
+  getChildHostContext() {
     return childHostContext;
   },
-  shouldSetTextContent: (type, props) => {
+  shouldSetTextContent(type, props) {
     return typeof props.children === 'string' || typeof props.children === 'number';
   },
-  createInstance: (type, newProps, rootContainerInstance, _currentHostContext, workInProgress) => {
+  createInstance(type, newProps, rootContainerInstance, _currentHostContext, workInProgress) {
     const domElement = document.createElement(type);
     Object.keys(newProps).forEach(propName => {
       const propValue = newProps[propName];
@@ -24,33 +27,30 @@ const hostConfig = {
         if (typeof propValue === 'string' || typeof propValue === 'number') {
           domElement.textContent = propValue;
         }
-      } else if (propName === 'onClick') {
-        domElement.addEventListener('click', propValue);
-      } else if (propName === 'className') {
+      } if (propName === 'className') {
         domElement.setAttribute('class', propValue);
-      } else {
+      } else if (propName !== 'children') {
         const propValue = newProps[propName];
         domElement.setAttribute(propName, propValue);
       }
     });
     return domElement;
   },
-  createTextInstance: text => {
+  createTextInstance(text) {
     return document.createTextNode(text);
   },
-  appendInitialChild: (parent, child) => {
+  appendInitialChild(parent, child) {
     parent.appendChild(child);
   },
   appendChild(parent, child) {
     parent.appendChild(child);
   },
-  finalizeInitialChildren: () => { },
-  supportsMutation: true,
-  appendChildToContainer: (parent, child) => {
-    parent.appendChild(child);
+  finalizeInitialChildren() {
+    return false;
   },
-  prepareUpdate(domElement, oldProps, newProps) {
-    return true;
+  supportsMutation: true,
+  appendChildToContainer(parent, child) {
+    parent.appendChild(child);
   },
   commitUpdate(domElement, updatePayload, type, oldProps, newProps) {
     Object.keys(newProps).forEach(propName => {
@@ -71,8 +71,11 @@ const hostConfig = {
   removeChild(parentInstance, child) {
     parentInstance.removeChild(child);
   },
-  clearContainer: () => { }
+  clearContainer(container) {
+    container.textContent = '';
+  }
 };
+
 const ReactReconcilerInst = ReactReconciler(hostConfig);
 
 const ReactDOMMini = {
